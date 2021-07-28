2484d8c1b821434b13051b008b94ea5c44ec8d69
kris23lis <kris23lis@gmail.com>
2021-07-28 14:20:41 +0300

    Init rep

---
+++ app/src/main/java/com/example/customview/ClockView.kt
@@ -0,0 +1,19 @@
+package com.example.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.View
+
+class ClockView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+
+
+}
